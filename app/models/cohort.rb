ca06fce8f8832dc46af83422103c003f33a4cca9
Matt Morrisey <mattydevicce@gmail.com>
2015-10-06 11:14:38 -0400

    Messed up the cohort model.. fixed it

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,5 +1,6 @@
 class Cohort < ActiveRecord::Base
-	has_and_belongs_to_many :quizzes, :users
+	has_and_belongs_to_many :quizzes
+	has_and_belongs_to_many :users
 	validates :name, :description, presence: true
 	validates :name, uniqueness: true
 end
