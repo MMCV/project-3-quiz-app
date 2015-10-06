58362d183fbdfcf4a172e8bcc91ab78a10a345e2
Meiji Chen <meiji.chen@gmail.com>
2015-10-06 11:13:22 -0400

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006142324) do
+ActiveRecord::Schema.define(version: 20151006144414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20151006142324) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "questions", force: :cascade do |t|
+    t.string   "question_text"
+    t.string   "question_answer"
+    t.integer  "quiz_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "questions", ["quiz_id"], name: "index_questions_on_quiz_id", using: :btree
+
   create_table "quizzes", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -30,6 +40,18 @@ ActiveRecord::Schema.define(version: 20151006142324) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
+
+  create_table "solutions", force: :cascade do |t|
+    t.text     "student_answer"
+    t.integer  "student_answer_score"
+    t.integer  "user_id"
+    t.integer  "question_id"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "solutions", ["question_id"], name: "index_solutions_on_question_id", using: :btree
+  add_index "solutions", ["user_id"], name: "index_solutions_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -41,4 +63,15 @@ ActiveRecord::Schema.define(version: 20151006142324) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users_cohorts", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "cohort_id"
+  end
+
+  add_index "users_cohorts", ["cohort_id"], name: "index_users_cohorts_on_cohort_id", using: :btree
+  add_index "users_cohorts", ["user_id"], name: "index_users_cohorts_on_user_id", using: :btree
+
+  add_foreign_key "questions", "quizzes"
+  add_foreign_key "solutions", "questions"
+  add_foreign_key "solutions", "users"
 end
