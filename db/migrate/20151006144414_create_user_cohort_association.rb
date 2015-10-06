5a8daa7d9c0605b16d19f4cc49a8fa2f3d12d6a6
Matt Morrisey <mattydevicce@gmail.com>
2015-10-06 12:51:00 -0400

    Fixed cohort user migration... cohort needs to be first since it was created first

--- db/migrate/20151006144414_create_user_cohort_association.rb
+++
@@ -1,8 +0,0 @@
-class CreateUserCohortAssociation < ActiveRecord::Migration
-  def change
-    create_table :users_cohorts, id: false do |t|
-    	t.belongs_to :user, index: true
-      t.belongs_to :cohort, index: true
-    end
-  end
-end
