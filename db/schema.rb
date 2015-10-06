141effc07b50c5ccb06f808df536e96dd4f3fccf
Matt Morrisey <mattydevicce@gmail.com>
2015-10-06 10:14:53 -0400

    merge models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006134303) do
+ActiveRecord::Schema.define(version: 20151006135642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,5 +22,13 @@ ActiveRecord::Schema.define(version: 20151006134303) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "users_cohorts", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "cohort_id"
+  end
+
+  add_index "users_cohorts", ["cohort_id"], name: "index_users_cohorts_on_cohort_id", using: :btree
+  add_index "users_cohorts", ["user_id"], name: "index_users_cohorts_on_user_id", using: :btree
 
 end
