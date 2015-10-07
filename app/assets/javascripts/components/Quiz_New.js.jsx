2f69a8dff9b618f0f607e0a2813e98eefa762232
Victoria Donnelly <vic.n.d1@gmail.com>
2015-10-07 10:56:45 -0400

    adds new quiz form, not working

--- app/assets/javascripts/components/Quiz_New.js.jsx
+++ app/assets/javascripts/components/Quiz_New.js.jsx
@@ -1,24 +1,78 @@
-var Quiz_New = React.createClass({
-  var questions = []
-  getInitialState: function(){
+// var Quiz_New = React.createClass({
+//   var questions = []
+//   getInitialState: function(){
+//
+//   };
+//
+//   render: function() {
+//     return (
+//       <form className="quiz-new">
+//         <input type="text" ref="question-text"></input>
+//         <input type="text" ref="question-answer"></input>
+//         <select>
+//           <option value="multiple-choice">Multiple Choice</option>
+//           <option value="text">Multiple Choice</option>
+//         </select>
+//         <input type="submit" value="submit"></input>
+//         <input type="submit" className="btn btn-default" value="add a question?"></input>
+//       </form>
+//     );
+//   }
+// });
+//
+// // push.question.array
+// refs in each input
+//
+// the input stacks down...on click pushes each
 
-  };
-
+var List = React.createClass({
   render: function() {
-    return (
-      <form className="quiz-new">
-        <input type="text" ref="question-text"></input>
-        <input type="text" ref="question-answer"></input>
-        <select>
-          <option value="multiple-choice">Multiple Choice</option>
-          <option value="text">Multiple Choice</option>
-        </select>
-        <input type="submit" value="submit"></input>
-        <input type="submit" className="btn btn-default" value="add a question?"></input>
-      </form>
+    return React.DOM.ul(null,
+      this.props.children.map(function(child, index){
+        return React.DOM.li(null,
+          child,
+          this.props.answer,
+          React.DOM.button({onClick: this.props.onDeleteItem.bind(null, index)}, "X")
+        );
+      }, this)
     );
   }
 });
 
-// push.question.array
-refs in each input
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      items: [],
+      answers: []
+    };
+  },
+
+  render: function() {
+     return (
+       <div>
+         <List onDeleteItem={this.onDeleteItem} answer={this.state.answers}>
+             {this.state.items}
+         </List>
+         <input ref='new_item' placeholder="Enter Question Here" />
+         <input ref='new_item_answer' placeholder="Enter Answer Here"/>
+         <button onClick={this.onAddClicked}>Add</button>
+       </div>
+     );
+   },
+
+  onAddClicked: function() {
+    var newItems = this.state.items.slice();
+    var newAnswers = this.state.answers.slice();
+    newAnswers.push(this.refs.new_item_answer.getDOMNode().value);
+    newItems.push(this.refs.new_item.getDOMNode().value);
+    this.setState({items: newItems, answers: newAnswers});
+  },
+
+  onDeleteItem: function(index) {
+    var newItems = this.state.items.slice();
+    var newAnswers = this.state.answers.slice();
+    newItems.splice(index, 1);
+    newAnswers.splice(index, 1);
+    this.setState({items: newItems, answers: newAnswers});
+  }
+});
