0ec353197ce416b37f89f26b36012944ac7d6392
Christopher Smith <smithopher87@gmail.com>
2015-10-08 15:28:40 -0400

    change to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008152744) do
+ActiveRecord::Schema.define(version: 20151008153807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 20151008152744) do
 
   add_index "cohorts_users", ["cohort_id"], name: "index_cohorts_users_on_cohort_id", using: :btree
   add_index "cohorts_users", ["user_id"], name: "index_cohorts_users_on_user_id", using: :btree
+
+  create_table "grades", force: :cascade do |t|
+    t.integer  "student_id"
+    t.integer  "quiz_id"
+    t.float    "grade"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "question_text"
