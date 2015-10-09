45e0e102d1e2f53501a5e43cf59b8a26f19bafa0
Matt Morrisey <mattydevicce@gmail.com>
2015-10-09 17:15:45 -0400

    Slight typo in sessions controller for instructor signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
 	  	if @user.type == 'Student'
 				redirect_to student_path(@user.id)
 			elsif @user.type == 'Instructor'
-				redirect_to instructor_path(@user.user_id)
+				redirect_to instructor_path(@user.id)
 			end
 	  else
 	  	redirect_to login_path
