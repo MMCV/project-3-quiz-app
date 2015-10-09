a850b34dfe549595e51198da52f9ca399d7eac91
Matt Morrisey <mattydevicce@gmail.com>
2015-10-09 12:18:26 -0400

    trash commit.. same changes were made in master.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
       session[:user_id] = user.id
-	  	redirect_to user_path(users)
+	  	redirect_to user_path(user)
 	  end
 	end
 
