c9fe3d0cba8bbf0a2344f620dfeae341a266fc1f
Victoria Donnelly <vic.n.d1@gmail.com>
2015-10-12 10:02:07 -0400

    reconciles users_controller file with master

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,27 +10,23 @@ class UsersController < ApplicationController
   	render component: 'UserNew'
   end
 
+  # UserMailer.welcome_email(user).deliver_now
+
   def create
-    if params[:type]=="Student"
-    	@student = Student.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-      if @student.save
-        session[:user_id] = @student.id
-        redirect_to student_path(@student)
-      end
-    else
-      user = Instructor.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-    end
-    if user.save
-      # UserMailer.welcome_email(user).deliver_now
-      session[:user_id] = user.id
-  	  render json: user
-      @instructor = Instructor.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-    if @instructor.save
-        session[:user_id] = @instructor.id
-        redirect_to instructor_path(@instructor)
-      end
-    end
-  end
+     if params[:type]=="Student"
+     	@student = Student.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+       if @student.save
+         session[:user_id] = @student.id
+         redirect_to student_path(@student)
+       end
+     else
+       @instructor = Instructor.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+       if @instructor.save
+         session[:user_id] = @instructor.id
+         redirect_to instructor_path(@instructor)
+       end
+     end
+   end
 
   def show
   	@user = User.find(params[:id])
