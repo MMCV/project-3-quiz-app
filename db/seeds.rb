02b0e11449df5fce7fd303152d445284059b1e4b
Meiji Chen <meiji.chen@gmail.com>
2015-10-13 09:11:08 -0400

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
-# User.create({first_name: 'Chris', last_name:'Smith', email: 'cs@ga.co', password: 'chrs', type: 'Student'});
-# User.create({first_name: 'Victoria', last_name:'Donnelly', email: 'vd@ga.co', password: 'vicd', type: 'Student'});
-# User.create({first_name: 'Matt', last_name:'Morrisey', email: 'mm@ga.co', password: 'matm', type: 'Student'});
-# User.create({first_name: 'Meiji', last_name:'Chen', email: 'mc@ga.co', password: 'meic', type: 'Student'});
-# User.create({first_name: 'Jaden', last_name:'Carver', email: 'jc@ga.co', password: 'jadc', type: 'Instructor'});
-# User.create({first_name: 'Pan', last_name:'Wangaperwong', email: 'pw@ga.co', password: 'panw', type: 'Instructor'});
-# User.create({first_name: 'Dennis', last_name:'Liaw', email: 'dl@ga.co', password: 'denl', type: 'Instructor'});
-# User.create({first_name: 'Bobby', last_name:'King', email: 'dl@ga.co', password: 'denl', type: 'Instructor'});
-
-
 cohorts = Cohort.create([
   {name: 'Pluto', description: FFaker::Lorem.sentence},
   {name: 'Bacon', description: FFaker::Lorem.sentence},
@@ -24,18 +5,30 @@ cohorts = Cohort.create([
   {name: 'Pizza', description: FFaker::Lorem.sentence},
   ]);
 
-  User.create({first_name: 'Chris', last_name:'Smith', email: 'cs@ga.co', password: 'chrs', type: 'Student', :cohorts => Cohort.where(:name => ['Pluto', 'Bacon'])});
-  User.create({first_name: 'Victoria', last_name:'Donnelly', email: 'vd@ga.co', password: 'vicd', type: 'Student', :cohorts => Cohort.where(:name => ['Pluto'])});
-  User.create({first_name: 'Matt', last_name:'Morrisey', email: 'mm@ga.co', password: 'matm', type: 'Student', :cohorts => Cohort.where(:name => ['Pluto'])});
-  User.create({first_name: 'Meiji', last_name:'Chen', email: 'mc@ga.co', password: 'meic', type: 'Student', :cohorts => Cohort.where(:name => ['Pluto'])});
-  User.create({first_name: 'Jaden', last_name:'Carver', email: 'jc@ga.co', password: 'jadc', type: 'Instructor', :cohorts => Cohort.where(:name => ['Pluto', 'Bacon', 'Pizza'])});
-  User.create({first_name: 'Pan', last_name:'Wangaperwong', email: 'pw@ga.co', password: 'panw', type: 'Instructor', :cohorts => Cohort.where(:name => ['Pluto', 'Steak', 'Pizza' ])});
-  User.create({first_name: 'Dennis', last_name:'Liaw', email: 'dl@ga.co', password: 'denl', type: 'Instructor', :cohorts => Cohort.where(:name => ['Pluto', 'Bacon', 'Pizza' ])});
-  User.create({first_name: 'Bobby', last_name:'King', email: 'dl@ga.co', password: 'denl', type: 'Instructor', :cohorts => Cohort.where(:name => ['Pluto', 'Pizza'])});
+  User.create({first_name: 'Chris', last_name:'Smith', email: 'cs@ga.co', password: 'chrs', type: 'Student', :cohorts => Cohort.where(:id=>['1','2','3','4'])});
+  User.create({first_name: 'Victoria', last_name:'Donnelly', email: 'vd@ga.co', password: 'vicd', type: 'Student', :cohorts => Cohort.where(:id=>['1','2','3','4'])});
+  User.create({first_name: 'Matt', last_name:'Morrisey', email: 'mm@ga.co', password: 'matm', type: 'Student', :cohorts => Cohort.where(:id=>['1','2','3','4'])});
+  User.create({first_name: 'Meiji', last_name:'Chen', email: 'mc@ga.co', password: 'meic', type: 'Student', :cohorts => Cohort.where(:id=>['1','2','3','4'])});
+  User.create({first_name: 'Jaden', last_name:'Carver', email: 'jc@ga.co', password: 'jadc', type: 'Instructor', :cohorts => Cohort.where(:id=>['1','2','3','4'])});
+  User.create({first_name: 'Pan', last_name:'Wangaperwong', email: 'pw@ga.co', password: 'panw', type: 'Instructor', :cohorts => Cohort.where(:id=>['1','2','3','4'])});
+  User.create({first_name: 'Dennis', last_name:'Liaw', email: 'dl@ga.co', password: 'denl', type: 'Instructor', :cohorts => Cohort.where(:id=>['1','2','3','4'])});
+  User.create({first_name: 'Bobby', last_name:'King', email: 'dl@ga.co', password: 'denl', type: 'Instructor', :cohorts => Cohort.where(:id=>['1','2','3','4'])});
 
 
 quizzes = Quiz.create([
   {name: FFaker::Lorem.word, description:FFaker::Lorem.sentence, assigned_date: "2015-09-01", :cohorts=>Cohort.where(:id=>['2','4'])},
-  {name: FFaker::Lorem.word, description:FFaker::Lorem.sentence, assigned_date: "2015-09-01"},
-  {name: FFaker::Lorem.word, description:FFaker::Lorem.sentence, assigned_date: "2015-09-01"},
+  {name: FFaker::Lorem.word, description:FFaker::Lorem.sentence, assigned_date: "2015-10-13", :cohorts=>Cohort.where(:id=>['1','2','3','4'])},
+  {name: FFaker::Lorem.word, description:FFaker::Lorem.sentence, assigned_date: "2015-09-01", :cohorts=>Cohort.where(:id=>['1','2','3','4'])},
+  {name: 'Test Pluto', description:FFaker::Lorem.sentence, assigned_date: "2015-10-13", :cohorts=>Cohort.where(:id=>['1','2','3','4'])},
+  {name: FFaker::Lorem.word, description:FFaker::Lorem.sentence, assigned_date: "2015-10-13", :cohorts=>Cohort.where(:id=>['1','2','3','4'])},
+  ]);
+
+questions = Question.create([
+  {question_text: FFaker::Lorem.sentence, question_answer: FFaker::Lorem.sentence, question_type: 'text', quiz_id:'4' },
+  {question_text: FFaker::Lorem.sentence, question_answer: FFaker::Lorem.sentence, question_type: 'text', quiz_id:'4' },
+  {question_text: FFaker::Lorem.sentence, question_answer: FFaker::Lorem.sentence, question_type: 'text', quiz_id:'4' },
+  {question_text: FFaker::Lorem.sentence, question_answer: FFaker::Lorem.sentence, question_type: 'text', quiz_id:'4' },
+  {question_text: FFaker::Lorem.sentence, question_answer: FFaker::Lorem.sentence, question_type: 'text', quiz_id:'2' },
+  {question_text: FFaker::Lorem.sentence, question_answer: FFaker::Lorem.sentence, question_type: 'text', quiz_id:'2' },
+  {question_text: FFaker::Lorem.sentence, question_answer: FFaker::Lorem.sentence, question_type: 'multiple-choice', quiz_id:'2', answer_1:FFaker::Lorem.word, answer_2:FFaker::Lorem.word,answer_3:FFaker::Lorem.word, answer_4:FFaker::Lorem.word},
   ]);
